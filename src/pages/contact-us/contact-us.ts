7b4c6a0d5b9862b241793ae9094b7141aacf9a35
daniel <danielk.kock@gmail.com>
2017-12-04 00:43:25 -0200

    email funcionando

--- src/pages/contact-us/contact-us.ts
+++ src/pages/contact-us/contact-us.ts
@@ -4,11 +4,11 @@ import { AlertController } from 'ionic-angular/components/alert/alert-controller
 import { EmailComposer } from '@ionic-native/email-composer';
 
 /**
- * Generated class for the ContactUsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+* Generated class for the ContactUsPage page.
+*
+* See https://ionicframework.com/docs/components/#navigation for more info on
+* Ionic pages and navigation.
+*/
 
 @IonicPage()
 
@@ -25,7 +25,7 @@ export class ContactUsPage {
   email: any;
   subject: any;
   emailSend: any;
-
+  
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -37,33 +37,32 @@ export class ContactUsPage {
       subject: 'Contato pelo Aplicativo',
       isHtml: true
     };
-
+    
     //this.emailSend.addAlias('gmail', 'com.google.android.gm');
     
   }
-
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactUsPage');
   }
-
+  
   public send() {
-    this.emailComposer.isAvailable().then((available: boolean) =>{
-      if(available) {
-        // Send a text message using default options
-        this.emailSend.body = "Nome: " + this.name + "\n Telefone: " + this.phone + "\n Email: " + this.email + "\n Mensagem: " + this.subject;
-        this.emailComposer.open(this.emailSend);
-      }
-     });
+    
+    // Send a text message using default options
+    this.emailSend.body = "Nome: " + this.name + "\n Telefone: " + this.phone + "\n Email: " + this.email + "\n Mensagem: " + this.subject;
+    this.emailComposer.open(this.emailSend);
+    this.doAlert();
+    
   }
-
+  
   doAlert() {
     let alert = this.alertCtrl.create({
       title: 'Mensagem enviada!',
       subTitle: 'Assim que possível entraremos em contato para resolver sua dúvida ou sugestão',
       buttons: ['Ok']
     });
-
+    
     alert.present();
   }
-
+  
 }
